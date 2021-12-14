9e0db67ead1dba7193495aeba4d8c96b72730b54
ProbIOU <probiou232@gmail.com>
2021-12-14 23:00:18 +0000

    configs

---
+++ libs/configs/ICDAR2015/probiou_l2/cfgs_res50_icdar2015_probiou_v1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+from libs.configs._base_.models.retinanet_r50_fpn import *
+from libs.configs._base_.datasets.dota_detection import *
+from libs.configs._base_.schedules.schedule_1x import *
+from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
+
+# schedule
+BATCH_SIZE = 1
+GPU_GROUP = "0"
+NUM_GPU = len(GPU_GROUP.strip().split(','))
+LR = 1e-3
+SAVE_WEIGHTS_INTE = 10000
+DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
+MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
+WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
+
+# dataset
+DATASET_NAME = 'ICDAR2015'
+IMG_SHORT_SIDE_LEN = 800
+IMG_MAX_LENGTH = 1000
+CLASS_NUM = 1
+
+# model
+pretrain_zoo = PretrainModelZoo()
+PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
+TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
+
+
+CLS_WEIGHT = 1.0
+REG_WEIGHT = 5.0
+# loss
+REG_LOSS_MODE = 10  # KLD loss
+
+
+# post-processing
+VIS_SCORE = 0.9
+
+VERSION = 'RetinaNet_ICDAR2015_PROBIOU_1x_20210204'
